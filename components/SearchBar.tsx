34ed79a75717dce048c3e0a618a4ea1ae24f9519
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-06-12 14:41:39 -0400

    able to display search bar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,42 @@
+import { SetStateAction, useState } from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+
+export const SearchBar = ({ onSearch }: any) => {
+    const [query, setQuery] = useState('');
+
+    const handleInputChange = (text: SetStateAction<string>) => {
+        setQuery(text);
+        if (onSearch) {
+            onSearch(text);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder="Search..."
+                value={query}
+                onChangeText={handleInputChange}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        borderRadius: 8,
+        borderColor: '#ddd',
+        borderWidth: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        elevation: 2,
+    },
+    input: {
+        height: 40,
+        fontSize: 16,
+    },
+})
+
