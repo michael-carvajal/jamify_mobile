df4bf69eb61f30187b0ffa0e6ceef94c1cb5a2c6
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-06-14 21:07:47 -0400

    added tyoes to best rated

--- components/BestRated.tsx
+++ components/BestRated.tsx
@@ -1,44 +1,44 @@
-import { FlatList, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { FlatList, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
 import { ThemedText } from './ThemedText';
 import { Colors } from '@/constants/Colors';
 import { ThemedView } from './ThemedView';
+import { Songsheet } from '../types/Songsheets';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 
-import { useNavigation } from '@react-navigation/native';
+interface BestRatedProps {
+  songSheets: Songsheet[];
+}
 
-const BestRated = ({ songSheets }: any) => {
-  const navigation = useNavigation();
+const BestRated: React.FC<BestRatedProps> = ({ songSheets }) => {
+  const navigation = useNavigation<NavigationProp<any>>();
 
-  const handlePress = (songSheet: any) => {
-    console.log('ipressed ittt');
-
+  const handlePress = (songSheet: Songsheet) => {
+    console.log('Pressed it');
     navigation.navigate('SongSheetDetails', { songSheet });
   };
 
   return (
-    <View
-      style={styles.container}
-    >
-      {songSheets.slice(17, 23).map((item, index: number) => (
-        <Pressable key={`best rated ${index}`} onPress={() => handlePress(item.title)}>
+    <View style={styles.container}>
+      {songSheets.slice(17, 23).map((item, index) => (
+        <Pressable key={`best rated ${index}`} onPress={() => handlePress(item)}>
           <ThemedView style={styles.item}>
             <ThemedText>{item.title}</ThemedText>
             {/* Render other song sheet details */}
           </ThemedView>
-        </Pressable>)
-      )}
+        </Pressable>
+      ))}
     </View>
   );
 };
 
-export default BestRated
+export default BestRated;
 
 const styles = StyleSheet.create({
   container: {
     alignSelf: 'flex-start',
     marginTop: 30,
-    width: "100%",
-
+    width: '100%',
   },
   item: {
     padding: 16,
@@ -47,4 +47,4 @@ const styles = StyleSheet.create({
     borderBottomColor: Colors.grey,
     flex: 1,
   },
-})
+});
