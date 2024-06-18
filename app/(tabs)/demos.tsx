bf10366f7c532c756b7a63f651fc02cc8963ea01
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-06-17 21:51:57 -0400

    able to record audio

--- app/(tabs)/demos.tsx
+++ app/(tabs)/demos.tsx
@@ -1,30 +1,88 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
-const Demos = () => {
-    const insets = useSafeAreaInsets();
+export default function Demos() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
 
-    return (
-        <ScrollView contentContainerStyle={{
-            justifyContent: 'center',
-            alignItems: 'center',
-        }} showsVerticalScrollIndicator={false} style={[styles.container, { paddingTop: insets.top}]}>
+  async function startRecording() {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        const { recording } = await Audio.Recording.createAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+        setRecording(recording);
+      }
+    } catch (err) {}
+  }
 
-            <ThemedView>
-                <ThemedText>Demos</ThemedText>
-            </ThemedView>
-        </ScrollView>
-    )
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+    let allRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    allRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    setRecordings(allRecordings);
+  }
+
+  function getDurationFormatted(milliseconds) {
+    const minutes = milliseconds / 1000 / 60;
+    const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
+    return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>
+            Recording #{index + 1} | {recordingLine.duration}
+          </Text>
+          <Button onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+        </View>
+      );
+    });
+  }
+
+  function clearRecordings() {
+    setRecordings([])
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title={recording ? 'Stop Recording' : 'Start Recording\n\n\n'} onPress={recording ? stopRecording : startRecording} />
+      {getRecordingLines()}
+      <Button title={recordings.length > 0 ? '\n\n\nClear Recordings' : ''} onPress={clearRecordings} />
+    </View>
+  );
 }
 
-export default Demos
-
 const styles = StyleSheet.create({
-    container: {
-      paddingHorizontal: 16,
-    },
-  
-  })
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 40
+  },
+  fill: {
+    flex: 1,
+    margin: 15
+  }
+});
