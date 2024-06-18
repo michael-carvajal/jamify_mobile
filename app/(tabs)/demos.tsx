f5571ca81ecbd1f1d41477fef4de994978337953
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-06-17 20:53:54 -0400

    added demo tab page

---
+++ app/(tabs)/demos.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Demos = () => {
+  return (
+    <View>
+      <Text>Demos</Text>
+    </View>
+  )
+}
+
+export default Demos
+
+const styles = StyleSheet.create({})
