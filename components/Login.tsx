256e3be7504b85fca48af67b0748688f895c09fe
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-06-23 09:36:36 -0400

    removed .env file and able to login on ios

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, TextInput, Button, Text, Platform, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Platform, Pressable, StyleSheet } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from 'expo-router';
 import { ThemedText } from './ThemedText';
 import { ThemedView } from './ThemedView';
 import InputField from './InputField';
 
 const Login = () => {
-    const navigation = useNavigation()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
@@ -17,33 +15,30 @@ const Login = () => {
     const apiUrl = env === "production" || Platform.OS === "ios" ? process.env.EXPO_PUBLIC_JAMIFY_API_URL : process.env.EXPO_PUBLIC_LOCAL_JAMIFY_API_URL;
 
     const parsedUrl = apiUrl?.split("/api")[0]
-    console.log(parsedUrl);
-    useEffect(() => {
-        handleLogin();
-    }, [])
+    // console.log(parsedUrl);
+
     const handleLogin = async () => {
-        const csrf = await AsyncStorage.getItem("csrf_token")
-        console.log(csrf);
+        // const csrf = await AsyncStorage.getItem("csrf_token")
+        // console.log(csrf);
         try {
 
-            const csrfToken = csrf
-            const response = await axios.post('http://127.0.0.1:5000/api/auth/login', {
+            // const csrfToken = csrf
+            const response = await axios.post(`${apiUrl!}/auth/login`, {
                 email: email,
                 password: password,
-            }, {
-                headers: {
-                    'X-CSRFToken': csrfToken
-                },
-                withCredentials: true, // Ensure credentials are sent with the request
             });
-            if (response.data.token) {
-                await AsyncStorage.setItem('authToken', response.data.token);
-            }
+            console.log("response.data ====> ", response);
+            
+            // if (response.data.token) {
+            //     await AsyncStorage.setItem('authToken', response.data.token);
+            // }
         } catch (error) {
             console.error('Login error', error);
         }
     };
 
+    // console.log(AsyncStorage.getItem('authToken'));
+    
     return (
         <View style={styles.container}>
             <InputField
