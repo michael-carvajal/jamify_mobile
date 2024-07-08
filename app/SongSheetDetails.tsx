d12fbe4dafb92e428ef0f4ff29d8ea193c5ce2f4
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-07-07 22:27:35 -0400

    added scroll view to songsheet details

--- app/SongSheetDetails.tsx
+++ app/SongSheetDetails.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 const SongSheetDetails = () => {
+  const insets = useSafeAreaInsets();
+  const colorScheme = useColorScheme();
+
   const route = useRoute();
   const { songSheet } = route.params;
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{songSheet.title}</Text>
-      {/* Add other song sheet details here */}
-    </View>
+
+    <ScrollView showsVerticalScrollIndicator={false} style={{  backgroundColor: Colors[colorScheme ?? 'light'].background }}>
+
+      <ThemedView style={styles.container}>
+        <ThemedView style={styles.header}>
+          <ThemedText style={styles.title}>{songSheet.title}</ThemedText>
+          <ThemedText style={{}}>{songSheet.title}</ThemedText>
+          <ThemedText style={{}}>{songSheet.title}</ThemedText>
+          <ThemedText style={{}}>{songSheet.title}</ThemedText>
+          <ThemedText style={{}}>{songSheet.title}</ThemedText>
+          <ThemedText style={{}}>{songSheet.title}</ThemedText>
+          {/* Add other song sheet details here */}
+        </ThemedView>
+      </ThemedView>
+    </ScrollView>
+
   );
 };
 
@@ -23,6 +42,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
+  header: {
+
+  }
 });
 
 export default SongSheetDetails;
